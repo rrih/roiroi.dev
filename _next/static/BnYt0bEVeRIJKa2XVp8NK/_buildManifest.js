be5af8638f8ab1ae5d79c5b4980c23536be49528
rrih <rrih@users.noreply.github.com>
2021-03-23 18:45:48 +0000

    deploy: 8f8348cf544d691cb473cb06ec5695f090ae6d7b

---
+++ _next/static/BnYt0bEVeRIJKa2XVp8NK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d60598e2363a4c1095cb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-56786c25c8429c83c24a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
